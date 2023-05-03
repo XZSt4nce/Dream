f5e555e430444be56e66180cc3f9a756681ad1dd
XZSt4nce <defeator@mail.ru>
2023-05-03 22:25:22 +0300

    ScanMazeTests template

---
+++ DreamTest/ScanMazeTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using dream;
+
+namespace DreamTest
+{
+    [TestClass]
+    public class ScanMaze
+    {
+        [TestMethod]
+        public void ScanMazeTest1()
+        {
+            char[,] maze = new char[,]
+            {
+                { 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+                { 'X', '.', 'X', '.', '.', 'E', 'X' },
+                { 'X', '.', 'X', '.', 'X', 'R', 'X' },
+                { 'X', 'X', 'X', '.', 'X', '.', 'X' },
+                { 'X', 'S', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
+            };
+            Dream test = new Dream(maze);
+        }
+    }
+}
