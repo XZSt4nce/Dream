5ffa59ac79f03c80aa82083ff6ea1e8d6b999da4
XZSt4nce <defeator@mail.ru>
2023-05-11 07:33:28 +0300

    imv

--- DreamTest/ScanNodesTests.cs
+++ DreamTest/ScanNodesTests.cs
@@ -42,9 +42,6 @@ namespace DreamTest
                 new Node(4, 5)
             };
 
-            expected[2].Colors[1] = 0b1000;
-            expected[6].Colors[1] = 0b1000;
-
             expected[0].AddNeighbor(expected[3]);
             expected[3].AddNeighbor(expected[0]);
             expected[1].AddNeighbor(expected[2]);
@@ -57,6 +54,145 @@ namespace DreamTest
             expected[2].AddNeighbor(expected[6]);
             expected[1].AddNeighbor(expected[5]);
             expected[5].AddNeighbor(expected[1]);
+
+            expected[2].Colors[1] = 0b1000;
+            expected[6].Colors[1] = 0b1000;
+
+            Assert.IsTrue(Compare(expected, test.Nodes.Values.ToArray()));
+        }
+
+        [TestMethod]
+        public void ScanNodesTest2()
+        {
+            char[,] maze = new char[,]
+            {
+                { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+                { 'X', 'S', '.', '.', 'Y', 'G', 'B', '.', '.', '.', '.', 'X' },
+                { 'X', '.', '.', 'X', 'X', 'X', 'R', 'X', 'X', 'X', '.', 'X' },
+                { 'X', '.', 'Y', 'G', '.', '.', 'E', 'Y', '.', '.', '.', 'X' },
+                { 'X', '.', 'X', '.', '.', '.', 'Y', '.', '.', '.', '.', 'X' },
+                { 'X', '.', 'X', 'X', 'X', '.', 'X', '.', '.', '.', '.', 'X' },
+                { 'X', '.', 'Y', '.', '.', '.', 'X', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', '.', 'X', 'X', 'X', 'X', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', '.', 'Y', 'G', '.', '.', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
+            };
+            Dream test = new Dream(maze);
+            test.ScanNodes();
+            Node[] expected = new Node[]
+            {
+                new Node(1, 1, 'S'),
+                new Node(1, 2),
+                new Node(1, 6),
+                new Node(1, 10),
+                new Node(2, 1),
+                new Node(2, 2),
+                new Node(3, 1),
+                new Node(3, 2),
+                new Node(3, 3),
+                new Node(3, 4),
+                new Node(3, 5),
+                new Node(3, 6, 'E'),
+                new Node(3, 7),
+                new Node(3, 8),
+                new Node(3, 9),
+                new Node(3, 10),
+                new Node(4, 3),
+                new Node(4, 4),
+                new Node(4, 5),
+                new Node(4, 6),
+                new Node(4, 7),
+                new Node(4, 8),
+                new Node(4, 9),
+                new Node(4, 10),
+                new Node(5, 7),
+                new Node(5, 8),
+                new Node(5, 9),
+                new Node(5, 10),
+                new Node(6, 1),
+                new Node(6, 2),
+                new Node(6, 5),
+                new Node(6, 7),
+                new Node(6, 8),
+                new Node(6, 9),
+                new Node(6, 10),
+                new Node(7, 7),
+                new Node(7, 8),
+                new Node(7, 9),
+                new Node(7, 10),
+                new Node(8, 2),
+                new Node(8, 7),
+                new Node(8, 8),
+                new Node(8, 9),
+                new Node(8, 10),
+            };
+
+            expected[1].AddNeighbor(expected[0]);
+            expected[0].AddNeighbor(expected[1]);
+            expected[2].AddNeighbor(expected[1]);
+            expected[1].AddNeighbor(expected[2]);
+            expected[3].AddNeighbor(expected[2]);
+            expected[2].AddNeighbor(expected[3]);
+            expected[4].AddNeighbor(expected[0]);
+            expected[0].AddNeighbor(expected[4]);
+            expected[4].AddNeighbor(expected[5]);
+            expected[5].AddNeighbor(expected[4]);
+            expected[5].AddNeighbor(expected[1]);
+            expected[1].AddNeighbor(expected[5]);
+            expected[4].AddNeighbor(expected[6]);
+            expected[6].AddNeighbor(expected[4]);
+            expected[7].AddNeighbor(expected[6]);
+            expected[6].AddNeighbor(expected[7]);
+            expected[7].AddNeighbor(expected[5]);
+            expected[5].AddNeighbor(expected[7]);
+            expected[7].AddNeighbor(expected[8]);
+            expected[8].AddNeighbor(expected[7]);
+            expected[9].AddNeighbor(expected[8]);
+            expected[8].AddNeighbor(expected[9]);
+            expected[10].AddNeighbor(expected[9]);
+            expected[9].AddNeighbor(expected[10]);
+            expected[11].AddNeighbor(expected[2]);
+            expected[2].AddNeighbor(expected[11]);
+            expected[12].AddNeighbor(expected[11]);
+            expected[11].AddNeighbor(expected[12]);
+            expected[13].AddNeighbor(expected[12]);
+            expected[12].AddNeighbor(expected[13]);
+            expected[14].AddNeighbor(expected[13]);
+            expected[13].AddNeighbor(expected[14]);
+            expected[15].AddNeighbor(expected[14]);
+            expected[14].AddNeighbor(expected[15]);
+            expected[15].AddNeighbor(expected[3]);
+            expected[3].AddNeighbor(expected[15]);
+            expected[16].AddNeighbor(expected[8]);
+            expected[8].AddNeighbor(expected[16]);
+            expected[17].AddNeighbor(expected[16]);
+            expected[16].AddNeighbor(expected[17]);
+            expected[17].AddNeighbor(expected[9]);
+            expected[9].AddNeighbor(expected[17]);
+            expected[18].AddNeighbor(expected[17]);
+            expected[17].AddNeighbor(expected[18]);
+            expected[18].AddNeighbor(expected[10]);
+            expected[10].AddNeighbor(expected[18]);
+            expected[19].AddNeighbor(expected[18]);
+            expected[18].AddNeighbor(expected[19]);
+            expected[19].AddNeighbor(expected[11]);
+            expected[11].AddNeighbor(expected[19]);
+            expected[20].AddNeighbor(expected[19]);
+            expected[19].AddNeighbor(expected[20]);
+            expected[20].AddNeighbor(expected[12]);
+            expected[12].AddNeighbor(expected[20]);
+            expected[21].AddNeighbor(expected[20]);
+            expected[20].AddNeighbor(expected[21]);
+            expected[21].AddNeighbor(expected[13]);
+            expected[13].AddNeighbor(expected[21]);
+            expected[22].AddNeighbor(expected[21]);
+            expected[21].AddNeighbor(expected[22]);
+            expected[22].AddNeighbor(expected[14]);
+            expected[14].AddNeighbor(expected[22]);
+            expected[23].AddNeighbor(expected[22]);
+            expected[22].AddNeighbor(expected[23]);
+            expected[23].AddNeighbor(expected[15]);
+            expected[15].AddNeighbor(expected[23]);
 
             Assert.IsTrue(Compare(expected, test.Nodes.Values.ToArray()));
         }
---
+++ DreamTest/SolveTests.cs
@@ -0,0 +1,52 @@
+﻿using dream;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DreamTest
+{
+    [TestClass]
+    public class SolveTests
+    {
+        [TestMethod]
+        public void SolveTest1()
+        {
+            char[,] maze = new char[,]
+            {
+                { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+                { 'X', 'S', '.', '.', 'Y', 'G', 'B', '.', '.', '.', '.', 'X' },
+                { 'X', '.', '.', 'X', 'X', 'X', 'R', 'X', 'X', 'X', '.', 'X' },
+                { 'X', '.', 'Y', 'G', '.', '.', 'E', 'Y', '.', '.', '.', 'X' },
+                { 'X', '.', 'X', '.', '.', '.', 'Y', '.', '.', '.', '.', 'X' },
+                { 'X', '.', 'X', 'X', 'X', '.', 'X', '.', '.', '.', '.', 'X' },
+                { 'X', '.', 'Y', '.', '.', '.', 'X', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', '.', 'X', 'X', 'X', 'X', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', '.', 'Y', 'G', '.', '.', '.', '.', '.', '.', 'X' },
+                { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }
+            };
+            Dream test = new Dream(maze);
+            test.R = 3;
+            test.G = 4;
+            test.B = 5;
+            test.Y = 6;
+            test.ScanNodes();
+            test.ScanPaths();
+            test.PathsColorization();
+            Assert.IsTrue(test.Paths.Count > 0, "Not enough paths");
+            Path first = test.Paths[0];
+            int min = Convert.ToInt32(first.Red) * test.R +
+                      Convert.ToInt32(first.Green) * test.G +
+                      Convert.ToInt32(first.Blue) * test.B +
+                      Convert.ToInt32(first.Yellow) * test.Y;
+            for (int i = 1; i < test.Paths.Count; i++)
+            {
+                Path path = test.Paths[i];
+                int cost = Convert.ToInt32(path.Red) * test.R +
+                           Convert.ToInt32(path.Green) * test.G +
+                           Convert.ToInt32(path.Blue) * test.B +
+                           Convert.ToInt32(path.Yellow) * test.Y;
+                if (cost < min) min = cost;
+            }
+            Assert.IsTrue(min == 10, "Wrong min cost");
+        }
+    }
+}
